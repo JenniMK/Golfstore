97f92c2d141ff37766bec0b4deb9bee1ca4c181b
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-05-18 16:50:53 +0300

    add category and directory components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-
+import Directory from './components/directory/directory.component';
+import clubsImage from './assets/clubs.jpeg';
+import womenImage from './assets/women.jpeg';
+import menImage from './assets/men.jpeg';
+import shoesImage from './assets/shoes.jpeg';
+import accessoriesImage from './assets/accessories.jpeg';
 
 const App = () => {
-
-const categories = [
-  {
-    id: 1,
-    title: 'Clubs',
-  },
-  {
-    id: 2,
-    title: 'Clothing women',
-  },
-  {
-    id: 3,
-    title: 'Clothing men',
-  },
-  {
-    id: 4,
-    title: 'Shoes',
-  },
-  {
-    id: 5,
-    title: 'Accessories',
+  const categories = [
+    {
+      id: 1,
+      title: 'Clubs',
+      imageUrl: clubsImage,
     },
-]
+    {
+      id: 2,
+      title: 'Clothing women',
+      imageUrl: womenImage,
+    },
+    {
+      id: 3,
+      title: 'Clothing men',
+      imageUrl: menImage,
+    },
+    {
+      id: 4,
+      title: 'Shoes',
+      imageUrl: shoesImage,
+    },
+    {
+      id: 5,
+      title: 'Accessories',
+      imageUrl: accessoriesImage,
+    },
+  ];
 
   return (
-    <div className='categories-container'>
-      {categories.map(({ title }) => (
-        <div className='category-container'>
-        {/*<img />*/}
-        <div className='category-body-container'>
-          <h2>{title}</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      ))}
-    
-    </div>
+    <Directory categories={categories} />
   )
 }
 export default App;
